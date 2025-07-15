655930aad530afc5e5bb3c8955d9e260a3b41b69
Paolo Tranquilli <redsun82@github.com>
2025-07-15 15:35:48 +0200

    Use nightly rust toolchain and rules_rust

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 
 set -ex
 
-bazel build @rules_rust//util/process_wrapper  # works
+bazel build @rules_rust//util/process_wrapper --@rules_rust//rust/toolchain/channel=nightly "$@" # works
 
-bazel build @rules_rust//util/process_wrapper --@rules_rust//rust/settings:experimental_use_cc_common_link  # broken with 1.88
+bazel build @rules_rust//util/process_wrapper --@rules_rust//rust/settings:experimental_use_cc_common_link --@rules_rust//rust/toolchain/channel=nightly "$@" # broken with 1.88
